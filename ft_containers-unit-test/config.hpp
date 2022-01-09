7081af43c70fd11831a0404fd528251a3994e4ad
epilar <epilar@ev-n6.kzn.21-school.ru>
2022-01-09 13:15:19 +0300

    errors_from_checker

--- ft_containers-unit-test/config.hpp
+++ ft_containers-unit-test/config.hpp
@@ -9,7 +9,7 @@
 // #define STACK			"../../../Stack.hpp"
 // #define MAP			  	"../../../Map.hpp"
 // #define SET			  	"../../../Set.hpp"
-// #define UTILITIES		"../../../Utility.hpp"
+#define UTILITIES		"../../../utility.hpp"
 
 /* comment this to turn OFF flags (-WWW, -std=98) */
 // #define FLAGS
@@ -17,13 +17,13 @@
 /**************************************************/
 /*		Change template names of your classes	  */
 /**************************************************/
-#define	_vector 			ft::Vector
-#define	_stack	 			ft::Stack
-#define	_map 				ft::Map
-#define	_set	 			ft::Set
-#define	_is_integral 		ft::is_integral
+#define	_vector 			ft::vector
+// #define	_stack	 			ft::Stack
+// #define	_map 				ft::Map
+// #define	_set	 			ft::Set
+// #define	_is_integral 		ft::is_integral
 #define	_enable_if 			ft::enable_if
-#define	_pair 				ft::pair
-#define	_make_pair 			ft::make_pair
+// #define	_pair 				ft::pair
+// #define	_make_pair 			ft::make_pair
 #define	_lexicographical	ft::lexicographical_compare
 #define	_equal				ft::equal
--- utilities.hpp
+++ utilities.hpp
@@ -38,6 +38,35 @@ namespace ft
 	};
 
 ///////////////////////////////////////////////////////////////////////////////
+//                              IS_INTEGRAL                                  //
+///////////////////////////////////////////////////////////////////////////////
+	template <class T, T v>
+		struct integral_constant
+		{
+			static const T					value = v;
+			typedef T						value_type;
+			typedef integral_constant<T, v>	type;
+			operator T() const { return v; }
+		};
+	
+	template <class T> struct is_integral : public integral_constant<T, false> {};
+	template <> struct is_integral<bool> : public integral_constant<bool, true> {};
+	template <> struct is_integral<char> : public integral_constant<char, true> {};
+	template <> struct is_integral<char16_t> : public integral_constant<char16_t, true> {};
+	template <> struct is_integral<char32_t> : public integral_constant<char32_t, true> {};
+	template <> struct is_integral<wchar_t> : public integral_constant<wchar_t, true> {};
+	template <> struct is_integral<signed char> : public integral_constant<signed char, true> {};
+	template <> struct is_integral<short int> : public integral_constant<short int, true> {};
+	template <> struct is_integral<int> : public integral_constant<int, true> {};
+	template <> struct is_integral<long int> : public integral_constant<long int, true> {};
+	template <> struct is_integral<long long int> : public integral_constant<long long int, true> {};
+	template <> struct is_integral<unsigned char> : public integral_constant<unsigned char, true> {};
+	template <> struct is_integral<unsigned short int> : public integral_constant<unsigned short int, true> {};
+	template <> struct is_integral<unsigned int> : public integral_constant<unsigned int, true> {};
+	template <> struct is_integral<unsigned long int> : public integral_constant<unsigned long int, true> {};
+	template <> struct is_integral<unsigned long long int> : public integral_constant<unsigned long long int, true> {};
+
+///////////////////////////////////////////////////////////////////////////////
 //                                ENABLE_IF                                  //
 ///////////////////////////////////////////////////////////////////////////////
 	template <bool B, class T = void>
@@ -84,6 +113,5 @@ namespace ft
 #endif
 
 
-//is_integral
 //std::pair
 //std::make_pair
